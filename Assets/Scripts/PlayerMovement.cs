93637a69ea389efb6737eb2e1cc308efd1f5eae0
SopitasWick <jesus.izaguirre216660@potros.itson.edu.mx>
2024-04-27 16:18:09 -0700

    Animacion personaje en escena y rotacion en eje x

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody myBody;
+
+    public float walk_Speed = 2f;
+    public float z_Speed = 1.5f;
+
+    private float rotation_Y = -98f;
+    private float rotation_Speed = 15f;
+
+    void Awake()
     {
-        
+        myBody = GetComponent<Rigidbody>();
+    }
+    void Update(){
+    RotatePlayer();
+    }
+    void FixedUpdate()
+    {
+        DetectMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    void DetectMovement()
     {
-        
+        myBody.velocity = new Vector3(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) * (-walk_Speed),
+                                      myBody.velocity.y,
+                                      Input.GetAxisRaw(Axis.VERTICAL_AXIS) * (-z_Speed));
     }
+    void RotatePlayer(){
+if(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS)> 0){
+transform.rotation = Quaternion.Euler(0f, rotation_Y, 0f);
+  }else if(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS)< 0){
+      transform.rotation = Quaternion.Euler(0f, Mathf.Abs(rotation_Y), 0f);
+    }
+  }
 }
+
